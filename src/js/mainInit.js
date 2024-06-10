6b637630411806f3d5fbfe6c639a8b156f4f3a63
jellemiddendorf <Jelle.middendorf@hotmail.com>
2024-06-10 13:46:03 +0200

    remove console logs small change

--- src/js/mainInit.js
+++ src/js/mainInit.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
+    console.log = function() {};
+    console.info = function() {};
+    console.warn = function() {};
+    console.error = function() {};
+    
     stateModule.init();
     stateMachineModule.init();
     ChatbotModule.init(stateModule.stateIDs, stateMachineModule.changeState);
     headerLoader.init();
 
     console.log("All modules initialized after DOM loaded.");
-
-    console.log = function() {};
-    console.info = function() {};
-    console.warn = function() {};
-    console.error = function() {};
 });
